e035ef43ff6a5547e118aee78735202bcdc43d72
Muhammad Zuhrul Umam <zuhrulu@gmail.com>
2026-01-29 17:11:58 +0700

    Update documentation link text to 'Documentation Update'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
                             <span>
                                 Read the
                                 <a href="https://laravel.com/docs" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
-                                    <span>Documentation</span>
+                                    <span>Documentation Update</span>
                                     <svg
                                         width="10"
                                         height="11"
